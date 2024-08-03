9b85ea0c405a0818a713a3878c92a641969e8078
r2hu1 <newasram86@gmail.com>
2024-08-03 15:14:29 +0530

    api

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,41 @@
+import User from "@/models/user";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from "@/lib/mongodb";
+import base64 from "base-64";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        try {
+          await connectDB();
+          const user = await User.findOne({ email });
+          if (!user) {
+            return null;
+          }
+          if (password != base64.decode(user.password)) {
+            return null;
+          }
+          return user;
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
