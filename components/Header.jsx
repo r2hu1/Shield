e0c790a5c767d25ad38f227b4ae83ace71a83cf4
r2hu1 <newasram86@gmail.com>
2024-08-03 15:15:42 +0530

    smple

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { ShieldHalf } from "lucide-react";
 import { Button } from "./ui/button";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
@@ -8,6 +9,7 @@ export default function Header(){
     console.log(user);
     return(
         <header className="px-6 py-5 flex items-center justify-between">
+            <ShieldHalf className="h-6 w-6"/>
             <Button onClick={()=>signOut({callbackUrl:'/login'})}>Logout</Button>
         </header>
     )
