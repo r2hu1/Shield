ab56732c11e85516b11ec1b9c1018ce8daeb8756
r2hu1 <newasram86@gmail.com>
2024-08-03 22:13:43 +0530

    ui

--- app/(auth)/verify-email/page.jsx
+++ app/(auth)/verify-email/page.jsx
@@ -15,23 +15,21 @@ export default function Page() {
             <div className="grid gap-2 mb-4 place-items-center">
                 <ShieldHalf className="h-8 w-8" />
                 <h1 className="text-lg -mb-2">Enter OTP</h1>
-                <p className="text-sm text-muted-foreground max-w-sm">Enter 6 digit one time password (OTP) sent to your email address!</p>
+                <p className="text-sm text-muted-foreground max-w-sm">Enter 4 digit one time password (OTP) sent to your email address!</p>
             </div>
             <div className="grid w-fit gap-3 mx-auto">
                 <InputOTP maxLength={6}>
                     <InputOTPGroup>
                         <InputOTPSlot index={0} />
                         <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
                     </InputOTPGroup>
-                    <InputOTPSeparator />
+                    <InputOTPSeparator/>
                     <InputOTPGroup>
+                        <InputOTPSlot index={2} />
                         <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
                     </InputOTPGroup>
                 </InputOTP>
-                <p className="text-sm text-muted-foreground">Didn't get? <span className="cursor-pointer hover:opacity-85 underline" onClick={()=>toast.success("OTP sent, check your email!")}>Resend</span></p>
+                <p className="text-sm text-muted-foreground">Didn't get? <span className="cursor-pointer hover:opacity-85 underline" onClick={() => toast.success("OTP sent, check your email!")}>Resend</span></p>
                 <Button>Verfy</Button>
             </div>
         </div>
