dbfc24f7bca2dc5a59b7508f85ada957999a28e6
r2hu1 <newasram86@gmail.com>
2024-08-03 15:14:25 +0530

    signup pge

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -2,33 +2,62 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Check, ShieldCheck, X } from "lucide-react";
+import { Check, Eye, EyeOff, ShieldCheck, ShieldHalf, X } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import { toast } from "sonner";
+import base64 from "base-64"
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function Page() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [viewType, setViewType] = useState("password");
     const reqMeets = /[A-Z]/.test(password) && /[0-9]/.test(password) && password.length >= 8 && /[!@#$%^&*(){}[\]/?|`~,.;:]/.test(password) ? true : false;
 
-    const handleSubmit = (e) => {
-        e.prevent.default();
-        if(!email){
-            return "ee"
+    const user = useSession();
+    const router = useRouter();
+
+    if (user.status === "authenticated") return router.push("/");
+    const handleSubmit = () => {
+        if (!email) {
+            return toast.error("Please enter your email address!");
         }
+        let encodedPwd = base64.encode(password);
+        fetch("/api/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password: encodedPwd }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (!data.success) {
+                    toast.error(data.error);
+                } else {
+                    toast.success("Account created successfully you can login now!");
+                    router.push("/login");
+                }
+            });
     };
     return (
         <div className="w-full max-w-md px-6">
             <div className="grid place-items-center text-center gap-2">
-                <ShieldCheck className="h-8 w-8" />
-                <p className="text-sm text-muted-foreground">SignUp using your email</p>
+                <ShieldHalf className="h-8 w-8" />
+                <h1 className="-mb-2.5 text-lg">SignUp to continue</h1>
+                <p className="text-sm text-muted-foreground">signup using your email address and password.</p>
             </div>
-            <form method="post" onSubmit={handleSubmit} className="mt-5 grid gap-3">
+            <div className="mt-8 grid gap-3">
                 <Label className="-mb-1" htmlFor="email">Email</Label>
                 <Input value={email} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="name@domain.com" type="email" />
                 <Label className="-mb-1" htmlFor="password">Password</Label>
-                <Input value={password} onChange={(e) => setPassword(e.target.value)} id="password" placeholder="********" type="password" />
-                {!reqMeets && password.length >= 1  ? (
+                <div className="flex items-center gap-2">
+                    <Input value={password} onChange={(e) => setPassword(e.target.value)} id="password" placeholder="********" type={viewType} />
+                    <Button size="icon" variant="secondary" onClick={() => setViewType(viewType === "password" ? "text" : "password")} className="min-w-10">{viewType != "password" ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}</Button>
+                </div>
+                {!reqMeets && password.length >= 1 ? (
                     <div className="mt-2 rounded-md border border-border p-3">
                         <ul className="list-none grid gap-2">
                             <li className="flex items-center gap-3">
@@ -49,14 +78,12 @@ export default function Page() {
                             </li>
                         </ul>
                     </div>
-                ) : (
-                    <p className="flex items-center gap-3 text-xs text-muted-foreground"><Check className="text-green-400 h-3.5 w-3.5"/> All requirements meet!</p>
-                )}
+                ) : null}
                 <div className="grid mt-3">
-                    <Button type="submit" disabled={!reqMeets}>SignUp</Button>
+                    <Button onClick={handleSubmit} disabled={!reqMeets}>SignUp</Button>
                     <p className="text-sm text-muted-foreground text-center mt-2">Already have one? <Link href="/login" className="underline text-primary">Login</Link></p>
                 </div>
-            </form>
+            </div>
         </div>
     );
 }
