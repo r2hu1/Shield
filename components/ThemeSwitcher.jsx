1f83acd658615543ac998dfe38402b3623cc6082
r2hu1 <newasram86@gmail.com>
2024-08-03 15:59:10 +0530

    sm

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { ChevronDown, ChevronRight, Monitor, Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -17,22 +17,21 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+      <DropdownMenuTrigger className="flex cursor-pointer select-none items-center justify-between rounded-sm px-2 py-1.5 text-sm outline-none transition-colors hover:bg-accent hover:text-accent-foreground w-full">
+        Theme <ChevronRight className="h-4 w-4"/>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+      <DropdownMenuContent align="end" className="mr-16 -mt-2">
+        <DropdownMenuItem className="flex items-center justify-between" onClick={() => setTheme("light")}>
           Light
+          <Sun className="h-4 w-4" />
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem className="flex items-center justify-between" onClick={() => setTheme("dark")}>
           Dark
+          <Moon className="h-4 w-4" />
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem className="flex items-center justify-between" onClick={() => setTheme("system")}>
           System
+          <Monitor className="h-4 w-4"/>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
