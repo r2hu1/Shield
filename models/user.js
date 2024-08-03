1be331f566db2dcc0544eb0ef381a3959f736a93
r2hu1 <newasram86@gmail.com>
2024-08-03 15:14:43 +0530

    sm fn

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import { Schema, models, model } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = models.User || model("User", userSchema);
+export default User;
