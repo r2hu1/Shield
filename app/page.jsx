a6ac786e4f7f1d65cf90e7f6d34dfdf901bd819e
r2hu1 <newasram86@gmail.com>
2024-08-03 15:26:20 +0530

    in dv

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,13 @@
+"use client";
 import Header from "@/components/Header";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+    const user = useSession();
+    const router = useRouter();
+    if (user.status == "unauthenticated") return router.push("/login");
+
     return (
         <main>
             <Header />
