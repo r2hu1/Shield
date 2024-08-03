ced53f224c79a032335833f2bbafa8c5ad36cf37
r2hu1 <newasram86@gmail.com>
2024-08-03 22:05:07 +0530

    ui

---
+++ app/(auth)/verify-email/page.jsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { ShieldHalf } from "lucide-react";
+
+export default function Page() {
+    return (
+        <div className="w-full max-w-md px-6 grid gap-3 text-center">
+            <div className="grid gap-2 mb-4 place-items-center">
+                <ShieldHalf className="h-8 w-8" />
+                <h1 className="text-lg -mb-2">Enter OTP</h1>
+                <p className="text-sm text-muted-foreground max-w-sm">Enter 6 digit one time password (OTP) sent to your email address!</p>
+            </div>
+            <div className="grid w-fit gap-3 mx-auto">
+                <InputOTP maxLength={6}>
+                    <InputOTPGroup>
+                        <InputOTPSlot index={0} />
+                        <InputOTPSlot index={1} />
+                        <InputOTPSlot index={2} />
+                    </InputOTPGroup>
+                    <InputOTPSeparator />
+                    <InputOTPGroup>
+                        <InputOTPSlot index={3} />
+                        <InputOTPSlot index={4} />
+                        <InputOTPSlot index={5} />
+                    </InputOTPGroup>
+                </InputOTP>
+                <Button>Verfy</Button>
+            </div>
+        </div>
+    )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,11 @@ import Header from "@/components/Header";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle, ExternalLink, ShieldAlert } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Page() {
     const user = useSession();
     const router = useRouter();
@@ -11,7 +16,21 @@ export default function Page() {
     return (
         <main>
             <Header />
-            Homepage
+            <div className="px-6 py-6 max-w-lg mx-auto w-fit">
+                <Alert>
+                    <ShieldAlert className="h-5 w-5" />
+                    <AlertTitle>Verification Pending!</AlertTitle>
+                    <AlertDescription className="flex gap-3 items-center justify-between">
+                        <p className="text-muted-foreground">
+                            Please verify your email address to use all settings and features.
+                        </p>
+                        <Button className="min-w-10" variant="outline" size="icon" asChild><Link href="/verify-email"><ExternalLink className="h-4 w-4" /></Link></Button>
+                    </AlertDescription>
+                </Alert>
+            </div>
+            <section className="px-6 py-20">
+
+            </section>
         </main>
     )
 }
