0e3a58a045358eff38ff04cb34afd77c1e46851a
r2hu1 <newasram86@gmail.com>
2024-08-03 15:26:02 +0530

    switcher

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,15 +2,16 @@
 import { ShieldHalf } from "lucide-react";
 import { Button } from "./ui/button";
 import { signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
+import { ModeToggle } from "./ThemeSwitcher";
 
-export default function Header(){
-    const user = useSession();
-    console.log(user);
-    return(
+export default function Header() {
+    return (
         <header className="px-6 py-5 flex items-center justify-between">
-            <ShieldHalf className="h-6 w-6"/>
-            <Button onClick={()=>signOut({callbackUrl:'/login'})}>Logout</Button>
+            <ShieldHalf className="h-6 w-6" />
+            <div className="flex items-center gap-2">
+                <Button onClick={() => signOut({ callbackUrl: '/login' })}>Logout</Button>
+                <ModeToggle />
+            </div>
         </header>
     )
 }
