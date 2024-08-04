8d270a6db24bb5ec436e389eaa46d264fc8ac054
r2hu1 <newasram86@gmail.com>
2024-08-04 21:25:07 +0530

    upd

--- server_functions/updateStatus.js
+++ server_functions/updateStatus.js
@@ -2,16 +2,15 @@
 
 import connectDB from "@/lib/mongodb";
 import User from "@/models/user";
-import base64 from "base-64";
 
 export default async function updateStatus({ currentUserEmail, newStatus }) {
     try {
         await connectDB();
         const user = await User.findOne({ email: currentUserEmail });
-        if (!user) return JSON.stringify({ error: 'User not found!' });
+        if (!user) return JSON.stringify({ success: false, error: "User not found" });
         const newUser = await User.findOneAndUpdate({ email: currentUserEmail }, { status: newStatus });
         if (newUser) return JSON.stringify({ success: true });
-        return JSON.stringify({ error: 'Something went wrong!' });
+        return JSON.stringify({ success: false, error: "Something went wrong" });
     }
     catch (e) {
         return JSON.stringify({ error: e });
