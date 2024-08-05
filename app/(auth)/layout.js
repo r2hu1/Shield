a51a7de58e32a5c23ee70b695ffaaffca60fc4be
r2hu1 <newasram86@gmail.com>
2024-08-05 13:55:22 +0530

    📝: added metadata

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "Authentication",
+    description: "login or sign up to continue using Shield.",
+}
 export default function Layout({ children }) {
     return (
         <section className="absolute h-full w-full flex items-center justify-center">
