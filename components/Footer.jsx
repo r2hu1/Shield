fc91b4680d329e4e70865b561837cf4fbcc00914
r2hu1 <newasram86@gmail.com>
2024-08-08 18:17:02 +0530

    👣: added built by and improved footer design

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,18 +3,19 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="grid place-items-center px-6 h-36 mt-3">
-            <div className="grid place-items-center text-center">
-                <div className="flex flex-col gap-1 items-center justify-center">
+        <footer className="mt-10 md:px-20 lg:px-32 px-7 mb-5">
+            <div className="grid gap-1">
+                <div className="flex items-center gap-1">
                     <ShieldHalf className="h-5 w-5 -ml-1" />
                     <h1 className="text-base">Shield</h1>
                 </div>
-                <p className="text-xs text-muted-foreground">end to end encrypted password manager</p>
-                <ul className="list-none flex items-center gap-3 mt-2">
-                    <li><Link href="https://github.com/r2hu1/shield" className="text-sm text-muted-foreground hover:underline">Github</Link></li>
-                    <li><Link href="https://github.com/r2hu1/shield/fork" className="text-sm text-muted-foreground hover:underline">Fork Repo</Link></li>
-                    <li><Link href="https://github.com/r2hu1/shield/issues" className="text-sm text-muted-foreground hover:underline">Open Issue</Link></li>
+                <p className="text-sm text-muted-foreground">opensource end to end encrypted password manager.</p>
+                <ul className="list-none flex gap-3 mt-2">
+                    <li><Link href="https://github.com/r2hu1/shield" className="text-sm text-muted-foreground underline underline-offset-4">Github</Link></li>
+                    <li><Link href="https://github.com/r2hu1/shield/fork" className="text-sm text-muted-foreground underline underline-offset-4">Fork Repo</Link></li>
+                    <li><Link href="https://github.com/r2hu1/shield/issues" className="text-sm text-muted-foreground underline underline-offset-4">Open Issue</Link></li>
                 </ul>
+                <p className="text-sm opacity-85 mt-3">Built by <Link className="underline underline-offset-4 text-muted-foreground" href="https://rahul.eu.org">r2hu1</Link> deployed on vercel.</p>
             </div>
         </footer>
     )
