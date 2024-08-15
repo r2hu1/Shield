445dc3e5eaa597bba91da21279b32cc2b3c181fc
r2hu1 <hi@rahul.eu.org>
2024-08-15 23:38:10 +0530

    some minor fixes

--- components/Passwords.jsx
+++ components/Passwords.jsx
@@ -4,17 +4,20 @@ import getPassword from "@/server_functions/pwd/getPassword";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
-import { AlertTriangle, Copy, Eye, Loader, RotateCw, Share2, Trash, X } from "lucide-react";
+import { AlertTriangle, Clipboard, Eye, Loader, RotateCw, Share2, Trash, X } from "lucide-react";
 import { decrypt } from "@/lib/crypto";
 import { FaCloudflare, FaFacebook, FaGithub, FaGoogle, FaInstagram, FaLinkedin, FaMicrosoft, FaQuora, FaRegUser, FaStackOverflow, FaTwitter, FaYoutube } from "react-icons/fa";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { toast } from "sonner";
@@ -125,7 +128,7 @@ export default function Passwords() {
                             <div className="flex items-center justify-between border border-border p-3 rounded-md">
                                 <div className="flex items-center">
                                     <div className="w-10 items-center flex justify-center mr-3">
-                                        {icons[item.name.toLowerCase()] || icons["account"]}
+                                        {icons[item.name.toLowerCase().split(" ")[0]] || icons["account"]}
                                     </div>
                                     <div>
                                         <h1 className="text-base">{item.name}</h1>
@@ -134,16 +137,16 @@ export default function Passwords() {
                                     </div>
                                 </div>
                                 <div>
-                                    <Dialog>
-                                        <DialogTrigger asChild>
+                                    <AlertDialog>
+                                        <AlertDialogTrigger>
                                             <Button className="min-w-10" size="icon" variant="outline">
                                                 <Eye className="h-4 w-4" />
                                             </Button>
-                                        </DialogTrigger>
-                                        <DialogContent>
-                                            <DialogHeader>
-                                                <DialogTitle className="text-left">Viewing {item.name} Account</DialogTitle>
-                                                <DialogDescription className="text-left">
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent>
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle className="text-left relative">Viewing {item.name} Account <AlertDialogCancel className="bg-none mt-0 mb-0 h-fit w-fit p-0 border-none hover:bg-transparent focus:bg-none -right-3 -top-3 absolute"><X className="h-4 w-4" /></AlertDialogCancel></AlertDialogTitle>
+                                                <AlertDialogDescription className="text-left">
                                                     <p className="text-sm text-muted-foreground">Copy, share or delete your {item.name} account.</p>
                                                     <div className="mt-5 grid gap-3">
                                                         <Label htmlFor="name" className="text-primary -mb-1">Name</Label>
@@ -151,12 +154,12 @@ export default function Passwords() {
                                                         <Label htmlFor="email" className="text-primary -mb-1">Email</Label>
                                                         <div className="flex items-center gap-2">
                                                             <Input type="text" id="email" value={decrypt(item.email)} readOnly className="w-full text-primary border-border" />
-                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.email)); toast.success(`Copied ${item.name} email to clipboard`) }}><Copy className="h-4 w-4" /></Button>
+                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.email)); toast.success(`Copied ${item.name} email to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
                                                         </div>
                                                         <Label htmlFor="password" className="text-primary -mb-1">Password <span className="text-xs text-muted-foreground">encrypted (click copy to decrypt & copy)</span></Label>
                                                         <div className="flex items-center gap-2">
                                                             <Input type="text" id="password" value={item.password} readOnly className="w-full text-primary border-border" />
-                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.password)); toast.success(`Copied ${item.name} password to clipboard`) }}><Copy className="h-4 w-4" /></Button>
+                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.password)); toast.success(`Copied ${item.name} password to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
                                                         </div>
                                                         <div className="flex items-center gap-2 mt-2">
                                                             <Button onClick={() => { navigator.clipboard.writeText(JSON.stringify({ "name": item.name, "email": decrypt(item.email), "password": decrypt(item.password) })); toast.success(`Copied ${item.name} to clipboard`) }}>Copy All</Button>
@@ -164,10 +167,10 @@ export default function Passwords() {
                                                             <Button onClick={() => { handleDelete(item._id) }} size="icon" variant="outline" className="text-red-400 border-red-400 hover:text-red-400" disabled={loading2}>{loading2 ? <Loader className="h-4 w-4 animate-spin" /> : <Trash className="h-4 w-4" />}</Button>
                                                         </div>
                                                     </div>
-                                                </DialogDescription>
-                                            </DialogHeader>
-                                        </DialogContent>
-                                    </Dialog>
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
                                 </div>
                             </div>
                         </div>
