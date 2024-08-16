9982a84db236e38d725767d7b909d5beed725f38
r2hu1 <hi@rahul.eu.org>
2024-08-16 23:49:54 +0530

    lg dev col 2

--- components/Passwords.jsx
+++ components/Passwords.jsx
@@ -129,80 +129,82 @@ export default function Passwords() {
                     <h1 className="text-lg font-medium flex items-center justify-between">Saved Passwords <Button onClick={() => window.location.reload()} size="icon" variant="outline" className="h-7 w-7 p-0"><RotateCw className="h-3.5 w-3.5" /></Button></h1>
                     <p className="text-sm mt-1 text-muted-foreground max-w-md">your saved passwords is encrypted and it can be decrypt only by you.</p>
                 </div>
-                {!loading ? data && data.map((item, i) => {
-                    return (
-                        <div key={i}>
-                            <div className="flex items-center justify-between border border-border p-3 rounded-md">
-                                <div className="flex items-center">
-                                    <div className="w-10 items-center flex justify-center mr-3">
-                                        {icons[item.name.toLowerCase().split(" ")[0]] || icons[item.name.toLowerCase().split(" ")[1]] || icons["account"]}
+                <div className="grid gap-3 md:grid-cols-2">
+                    {!loading ? data && data.map((item, i) => {
+                        return (
+                            <div key={i}>
+                                <div className="flex items-center justify-between border border-border p-3 rounded-md">
+                                    <div className="flex items-center">
+                                        <div className="w-10 items-center flex justify-center mr-3">
+                                            {icons[item.name.toLowerCase().split(" ")[0]] || icons[item.name.toLowerCase().split(" ")[1]] || icons["account"]}
+                                        </div>
+                                        <div>
+                                            <h1 className="text-base">{item.name}</h1>
+                                            <p className="text-xs text-muted-foreground">{decrypt(item.email)}</p>
+                                            {/* <span className="text-sm">Strength {passStrength(decrypt(item.password))}%</span> */}
+                                        </div>
                                     </div>
                                     <div>
-                                        <h1 className="text-base">{item.name}</h1>
-                                        <p className="text-xs text-muted-foreground">{decrypt(item.email)}</p>
-                                        {/* <span className="text-sm">Strength {passStrength(decrypt(item.password))}%</span> */}
+                                        <AlertDialog>
+                                            <AlertDialogTrigger>
+                                                <Button className="min-w-10" size="icon" variant="outline">
+                                                    <Eye className="h-4 w-4" />
+                                                </Button>
+                                            </AlertDialogTrigger>
+                                            <AlertDialogContent>
+                                                <AlertDialogHeader>
+                                                    <AlertDialogTitle className="text-left relative">Viewing {item.name} Account <AlertDialogCancel className="bg-none mt-0 mb-0 h-fit w-fit p-0 border-none hover:bg-transparent focus:bg-none -right-3 -top-3 absolute"><X className="h-4 w-4" /></AlertDialogCancel></AlertDialogTitle>
+                                                    <AlertDialogDescription className="text-left">
+                                                        <p className="text-sm text-muted-foreground">Copy, share or delete your {item.name} account.</p>
+                                                        <div className="mt-5 grid gap-3">
+                                                            <Label htmlFor="name" className="text-primary -mb-1">Name</Label>
+                                                            <Input type="text" id="name" value={item.name} readOnly className="w-full text-primary border-border" />
+                                                            <Label htmlFor="email" className="text-primary -mb-1">Email</Label>
+                                                            <div className="flex items-center gap-2">
+                                                                <Input type="text" id="email" value={decrypt(item.email)} readOnly className="w-full text-primary border-border" />
+                                                                <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.email)); toast.success(`Copied ${item.name} email to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
+                                                            </div>
+                                                            <Label htmlFor="password" className="text-primary -mb-1">Password <span className="text-xs text-muted-foreground">encrypted (click copy to decrypt & copy)</span></Label>
+                                                            <div className="flex items-center gap-2">
+                                                                <Input type="text" id="password" value={item.password} readOnly className="w-full text-primary border-border" />
+                                                                <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.password)); toast.success(`Copied ${item.name} password to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
+                                                            </div>
+                                                            <div className="flex items-center gap-2 mt-2">
+                                                                <Button onClick={() => { navigator.clipboard.writeText(JSON.stringify({ "name": item.name, "email": decrypt(item.email), "password": decrypt(item.password) })); toast.success(`Copied ${item.name} to clipboard`) }}>Copy All</Button>
+                                                                <Button size="icon" onClick={() => navigator.share({ "title": `${item.name} Account`, "text": `Name: ${item.name}\nEmail: ${decrypt(item.email)}\nPassword: ${decrypt(item.password)}` })} variant="outline" className="text-primary border-primary"><Share2 className="h-4 w-4" /></Button>
+                                                                <Popover>
+                                                                    <PopoverTrigger>
+                                                                        <Button size="icon" variant="outline" className="text-red-400 border-red-400 hover:text-red-400"><Trash className="h-4 w-4" /></Button>
+                                                                    </PopoverTrigger>
+                                                                    <PopoverContent>
+                                                                        <p className="text-sm text-muted-foreground mb-3">
+                                                                            Are you sure you want to delete <span className="text-primary underline">{item.name}?</span> <br /> This action cannot be undone.
+                                                                        </p>
+                                                                        <Button onClick={() => { handleDelete(item._id) }} disabled={loading2} size="sm" variant="destructive">{loading2 ? <Loader className="h-4 w-4 animate-spin" /> : "Delete"}</Button>
+                                                                    </PopoverContent>
+                                                                </Popover>
+                                                            </div>
+                                                        </div>
+                                                    </AlertDialogDescription>
+                                                </AlertDialogHeader>
+                                            </AlertDialogContent>
+                                        </AlertDialog>
                                     </div>
                                 </div>
-                                <div>
-                                    <AlertDialog>
-                                        <AlertDialogTrigger>
-                                            <Button className="min-w-10" size="icon" variant="outline">
-                                                <Eye className="h-4 w-4" />
-                                            </Button>
-                                        </AlertDialogTrigger>
-                                        <AlertDialogContent>
-                                            <AlertDialogHeader>
-                                                <AlertDialogTitle className="text-left relative">Viewing {item.name} Account <AlertDialogCancel className="bg-none mt-0 mb-0 h-fit w-fit p-0 border-none hover:bg-transparent focus:bg-none -right-3 -top-3 absolute"><X className="h-4 w-4" /></AlertDialogCancel></AlertDialogTitle>
-                                                <AlertDialogDescription className="text-left">
-                                                    <p className="text-sm text-muted-foreground">Copy, share or delete your {item.name} account.</p>
-                                                    <div className="mt-5 grid gap-3">
-                                                        <Label htmlFor="name" className="text-primary -mb-1">Name</Label>
-                                                        <Input type="text" id="name" value={item.name} readOnly className="w-full text-primary border-border" />
-                                                        <Label htmlFor="email" className="text-primary -mb-1">Email</Label>
-                                                        <div className="flex items-center gap-2">
-                                                            <Input type="text" id="email" value={decrypt(item.email)} readOnly className="w-full text-primary border-border" />
-                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.email)); toast.success(`Copied ${item.name} email to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
-                                                        </div>
-                                                        <Label htmlFor="password" className="text-primary -mb-1">Password <span className="text-xs text-muted-foreground">encrypted (click copy to decrypt & copy)</span></Label>
-                                                        <div className="flex items-center gap-2">
-                                                            <Input type="text" id="password" value={item.password} readOnly className="w-full text-primary border-border" />
-                                                            <Button className="min-w-10" size="icon" onClick={() => { navigator.clipboard.writeText(decrypt(item.password)); toast.success(`Copied ${item.name} password to clipboard`) }}><Clipboard className="h-4 w-4" /></Button>
-                                                        </div>
-                                                        <div className="flex items-center gap-2 mt-2">
-                                                            <Button onClick={() => { navigator.clipboard.writeText(JSON.stringify({ "name": item.name, "email": decrypt(item.email), "password": decrypt(item.password) })); toast.success(`Copied ${item.name} to clipboard`) }}>Copy All</Button>
-                                                            <Button size="icon" onClick={() => navigator.share({ "title": `${item.name} Account`, "text": `Name: ${item.name}\nEmail: ${decrypt(item.email)}\nPassword: ${decrypt(item.password)}` })} variant="outline" className="text-primary border-primary"><Share2 className="h-4 w-4" /></Button>
-                                                            <Popover>
-                                                                <PopoverTrigger>
-                                                                    <Button size="icon" variant="outline" className="text-red-400 border-red-400 hover:text-red-400"><Trash className="h-4 w-4" /></Button>
-                                                                </PopoverTrigger>
-                                                                <PopoverContent>
-                                                                    <p className="text-sm text-muted-foreground mb-3">
-                                                                        Are you sure you want to delete <span className="text-primary underline">{item.name}?</span> <br /> This action cannot be undone.
-                                                                    </p>
-                                                                    <Button onClick={() => { handleDelete(item._id) }} disabled={loading2} size="sm" variant="destructive">{loading2 ? <Loader className="h-4 w-4 animate-spin" /> : "Delete"}</Button>
-                                                                </PopoverContent>
-                                                            </Popover>
-                                                        </div>
-                                                    </div>
-                                                </AlertDialogDescription>
-                                            </AlertDialogHeader>
-                                        </AlertDialogContent>
-                                    </AlertDialog>
-                                </div>
                             </div>
+                        )
+                    }) : (
+                        <div className="grid gap-3">
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
+                            <Skeleton className="h-16 rounded-md w-full" />
                         </div>
-                    )
-                }) : (
-                    <div className="grid gap-3">
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                        <Skeleton className="h-16 rounded-md w-full" />
-                    </div>
-                )}
+                    )}
+                </div>
                 {!loading && data.length == 0 && (
                     <div className="flex border-border border rounded-md items-center justify-center h-64">
                         <div className="grid place-items-center text-center">
