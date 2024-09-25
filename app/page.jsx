021be389c5e357be8da6cc324c67ae8efa560fb0
Rahul Rajput <newasram86@gmail.com>
2024-09-25 22:47:52 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -49,7 +49,7 @@ export default function Page() {
                 <div className="flex items-center justify-between gap-3 bg-secondary/40 p-3">
                     <p className="text-sm text-foreground/80">Download the shield app for better experience!</p>
                     <Button size="sm" className="text-xs p-0 h-8 px-3" asChild>
-                        <Link href="/shield.apk">Download</Link>
+                        <Link href="https://github.com/r2hu1/Shield/raw/refs/heads/main/public/shield.apk">Download</Link>
                     </Button>
                 </div>
             )}
