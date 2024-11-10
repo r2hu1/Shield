393287836bdfc0302e123484718f3d67d3325190
Rahul Rajput <newasram86@gmail.com>
2024-11-10 20:33:51 +0530

    Update Notif.jsx

--- components/Notif.jsx
+++ components/Notif.jsx
@@ -14,7 +14,7 @@ export default function Notif() {
     }, []);
     return !inPwa && (
         <div className="flex items-center justify-between gap-3 bg-secondary/50 px-3 py-3 md:px-20 lg:px-32">
-            <p className="text-xs text-foreground/80">Download the shield mobile app for better experience!</p>
+            <p className="text-sm text-foreground/80">Download the Shield app for better experience!</p>
             <Button size="sm" className="text-xs p-0 h-8 px-3" asChild>
                 <Link href="https://github.com/r2hu1/Shield/raw/refs/heads/main/public/shield.apk">Download</Link>
             </Button>
