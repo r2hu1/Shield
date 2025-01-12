8dd990c0f89bc6a5da60bc80935a98825b11c3d5
r2hu1 <hi@rahul.eu.org>
2025-01-12 22:32:57 +0530

    minimal

--- components/Preloader.jsx
+++ components/Preloader.jsx
@@ -9,14 +9,11 @@ export default function Preloader() {
         setTimeout(() => {
             setShow(false);
             document.body.style.overflow = "auto";
-        },2000);
-    },[]);
+        }, 2000);
+    }, []);
     return show && (
         <div className="fixed z-[99999999] top-0 left-0 right-0 bottom-0 h-full w-full flex items-center justify-center bg-background">
-            <div className="grid place-items-center">
-                <Loader className="h-5 w-5 animate-spin" />
-                <p className="text-sm text-center mt-1 text-muted-foreground">Hold on! loading..</p>
-            </div>
+            <Loader2 className="h-6 w-6 animate-spin" />
         </div>
     )
 }
