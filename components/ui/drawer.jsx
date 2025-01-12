5f20b4be7fed9b510b5336643c8747a07c3297fa
r2hu1 <hi@rahul.eu.org>
2025-01-12 14:13:09 +0530

    slite modif

--- components/ui/drawer.jsx
+++ components/ui/drawer.jsx
@@ -37,7 +37,7 @@ const DrawerContent = React.forwardRef(({ className, children, ...props }, ref) 
         className
       )}
       {...props}>
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-muted" />
+      <div className="mx-auto mt-2 h-1 w-[80px] rounded-full bg-muted" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
